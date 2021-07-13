eeffd7b7a14bf3a0a47283e4cbeb53139e44ecad
ajpneto <neto.ajp@gmail.com>
2021-07-12 21:53:33 -0300

    update

--- static/nav-bar.js
+++ static/nav-bar.js
@@ -1,33 +1,23 @@
 $(function () {
     var bar = '';
-    bar += '<nav class="navbar navbar-default" role="navigation">';
-    bar += '<div class="container-fluid">';
-    bar += '<div>';
-    bar += '<ul class="nav navbar-nav">';
-    bar += '<li id="blog"><a href="home.html">Articles</a></li>';
-    bar += '</ul>';
+    bar+= '<div class="topnav" id="myTopnav">';
+    bar += '<a href="home.html" class="active">Home</a>';
+    bar += '<a href="news.html">News</a>';
+    bar += '<a href="about.html">About</a>';
+    bar += '<a href="javascript:void(0);" class="icon" onclick="myFunction()">';
+    bar += '<i class="fa fa-bars"></i>';
+    bar += '</a>';
     bar += '</div>';
-    bar += '</div>';
-    bar += '</nav>';
 
     $("#main-bar").html(bar);
-
-    var id = getValueByName("id");
-    $("#" + id).addClass("active");
 });
 
-function getValueByName(name) {
-    var url = document.getElementById('nav-bar').getAttribute('src');
-    var param = new Array();
-    if (url.indexOf("?") != -1) {
-        var source = url.split("?")[1];
-        items = source.split("&");
-        for (var i = 0; i < items.length; i++) {
-            var item = items[i];
-            var parameters = item.split("=");
-            if (parameters[0] == "id") {
-                return parameters[1];
-            }
-        }
-    }
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
+
