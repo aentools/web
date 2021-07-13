19cb3f1175a00a92e5534f2688581886e4e56615
ajpneto <neto.ajp@gmail.com>
2021-07-13 10:37:57 -0300

    update

--- static/nav-bar.js
+++ static/nav-bar.js
@@ -2,8 +2,9 @@ $(function () {
     var bar = '';
     bar+= '<div class="topnav" id="myTopnav">';
     bar += '<a href="index.html" class="active">Home</a>';
+    bar += '<a href="about.html">About Us</a>';
     bar += '<a href="news.html">News</a>';
-    bar += '<a href="about.html">About</a>';
+    bar += '<a href="news.html">Projects</a>';
     bar += '<a href="README.md">Contact</a>';
     bar += '<a href="javascript:void(0);" class="icon" onclick="myFunction()">';
     bar += '<i class="fa fa-bars"></i>';
