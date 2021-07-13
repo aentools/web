0bc0632a7dc16df800d06f2876f7c01171e5c437
ajpneto <neto.ajp@gmail.com>
2021-07-13 13:30:41 -0300

    update

--- static/nav-bar.js
+++ static/nav-bar.js
@@ -4,7 +4,7 @@ $(function () {
     bar += '<a href="index.html" class="active">Home</a>';
     bar += '<a href="about.html">About Us</a>';
     bar += '<a href="news.html">News</a>';
-    bar += '<a href="news.html">Projects</a>';
+    bar += '<a href="https://aentools.herokuapp.com/projects/">Projects</a>';
     bar += '<a href="README.md">Contact</a>';
     bar += '<a href="javascript:void(0);" class="icon" onclick="myFunction()">';
     bar += '<i class="fa fa-bars"></i>';
