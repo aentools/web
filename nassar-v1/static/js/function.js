cdfd8f986bdfdc1d83eaeb6de700965f8fdf1825
aentools <ajpneto@ufabc.edu.br>
2021-08-01 15:50:01 -0300

    update

---
+++ nassar-v1/static/js/function.js
@@ -0,0 +1,32 @@
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("nav-bar").style.padding = "30px 10px";
+    document.getElementById("brand").style.fontSize = "25px";
+  } else {
+    document.getElementById("nav-bar").style.padding = "80px 10px";
+    document.getElementById("brand").style.fontSize = "35px";
+  }
+}
+
+(function($) {
+  $(function() {
+    $('nav ul li a:not(:only-child)').click(function(e) {
+      $(this).siblings('.nav-dropdown').toggle();
+      $('.dropdown').not($(this).siblings()).hide();
+      e.stopPropagation();
+    });
+    $('html').click(function() {
+      $('.nav-dropdown').hide();
+    });
+    $('#nav-toggle').click(function() {
+      $('nav ul').slideToggle();
+    });
+    $('#nav-toggle').on('click', function() {
+      this.classList.toggle('active');
+    });
+  });
+})(jQuery);
+
