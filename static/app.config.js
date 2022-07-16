5b54befd3461456afbb6db13da363ab329d1cc4e
aentools <86806835+aentools@users.noreply.github.com>
2022-07-16 18:42:27 -0300

    Update app.config.js

--- static/app.config.js
+++ static/app.config.js
@@ -2,9 +2,9 @@ angular.
   module('phonecatApp').
   config(['$routeProvider',
      function config($routeProvider) {
-        $routeProvider.when('/phones', {
+        $routeProvider.when('/', {
            template: '<phone-list></phone-list>'
      }).
-     otherwise('/phones');
+     otherwise('/');
  }
 ]);
