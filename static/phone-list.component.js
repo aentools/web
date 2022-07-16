979852dafc88603a51b385ba9ec2da2ae3c13b2a
aentools <86806835+aentools@users.noreply.github.com>
2022-07-16 13:58:37 -0300

    Update phone-list.component.js

--- static/phone-list.component.js
+++ static/phone-list.component.js
@@ -6,6 +6,7 @@ template:
 '<div class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">'+
 '<div class="col mb-5" ng-repeat="phone in $ctrl.phones">'+
 '<div class="card h-100">'+
+'<div class="badge bg-dark text-white position-absolute" style="top: 0.5rem; right: 0.5rem">Sale</div>'+
 '<!-- Product image-->'+
 '<img class="card-img-top" src="{{phone.imgUrl}}" alt="..." />'+
 '<!-- Product details-->'+
